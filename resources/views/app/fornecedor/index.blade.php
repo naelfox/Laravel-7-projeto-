73766222db436caf629ac6cb970f93237431475b
Naelfox <natanaelvila2@gmail.com>
2023-07-13 17:45:50 -0300

    .

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -12,11 +12,11 @@ echo "Olá";
 
 
 @if(count($fornecedores) > 0 && count($fornecedores) < 10)
-<h3>Existem alguns fornecedores cadastrados</h3>
+    <h3>Existem alguns fornecedores cadastrados</h3>
 @elseif(count($fornecedores) > 10)
-<h3>Existem vários fornecedores cadastrados</h3>
+    <h3>Existem vários fornecedores cadastrados</h3>
 @else
-<h3>Ainda não existem fornecedores cadastrados</h3>
+    <h3>Ainda não existem fornecedores cadastrados</h3>
 @endif
 
 

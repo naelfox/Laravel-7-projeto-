e93d1c8e63ca7c807d274cf6b8882ff8e6ffadb0
naelfox <natanaelvila2@gmail.com>
2023-10-21 17:07:44 -0300

    blade e migrations

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,6 +9,9 @@ class ContatoController extends Controller
     //
 
     public function contato() {
+        // echo '<pre>';
+        // print_r($_POST);
+        // echo '</pre>';
         return view('site.contato', ['titulo' => 'Contato (controller)']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', 'PrincipalController@principal')->name('site.index');
 
 Route::get('/sobre-nos',  'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato',  'ContatoController@contato')->name('site.contato');
+Route::match(['get', 'post'], '/contato',  'ContatoController@contato')->name('site.contato');
+
 Route::get('/login',  function(){
     return 'Login';
 })->name('login');

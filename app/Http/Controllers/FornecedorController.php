fec2101c7518c2d6b319dba744dc1489d9498652
naelfox <natanaelvila2@gmail.com>
2023-10-21 01:09:52 -0300

    .

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,8 +9,26 @@ class FornecedorController extends Controller
     public function index()
     {
         $fornecedores = [
-            0 => ['nome' => 'fornecedor 1', 'status' => 'N']
+            [
+                'nome' => 'Fornecedor 1',
+                'endereco' => 'Rua Fornecedor 1, Cidade 1',
+                'telefone' => '123-456-7890',
+                'email' => 'fornecedor1@example.com'
+            ],
+            [
+                'nome' => 'Fornecedor 2',
+                'endereco' => 'Rua Fornecedor 2, Cidade 2',
+                'telefone' => '987-654-3210',
+                'email' => 'fornecedor2@example.com'
+            ],
+            [
+                'nome' => 'Fornecedor 3',
+                'endereco' => 'Rua Fornecedor 3, Cidade 3',
+                'telefone' => '555-555-5555',
+                'email' => 'fornecedor3@example.com'
+            ]
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
+        // return view('app.fornecedor.index');
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,19 +4,60 @@
 
 @php
 
-echo "Olá";
+    echo 'Olá';
 
 @endphp
+<br>
 
-@dd($fornecedores)
+@empty($fornecedores)
+    array vazio
+@endempty
 
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+{{-- @dd($fornecedores) --}}
+@forelse ($fornecedores as $fornecedor)
+    Iteração atual: {{ $loop->iteration }} <br>
+    Primeira iteração: @if($loop->first)
+                        Sim
+                        @else
+                        Não
+                        @endif
+    Fornecedor {{ $fornecedor['nome'] }} <br>
+    Endereço {{ $fornecedor['endereco'] }} <br><br>
+@empty
+    Não existem fornecedores cadastrados
+@endforelse
+
+
+
+{{-- @empty($fornecedores[0]['cnpj'])
+    Vazio
+@endempty
+
+@isset($fornecedores[0]['empresa'])
+    <h2> {{ $fornecedores[0]['empresa'] }}</h2>
+@endisset
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Satatus: {{ $fornecedores[0]['status'] }}
+<br>
+@if (!($fornecedores[0]['status'] == 'S'))
+    Fornecedor inativo
+@endif
+
+@unless ($fornecedores[0]['status'] == 'S')
+    Fornecedor inativo
+@endunless --}}
+
+
+
+{{--
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem alguns fornecedores cadastrados</h3>
 @elseif(count($fornecedores) > 10)
     <h3>Existem vários fornecedores cadastrados</h3>
 @else
     <h3>Ainda não existem fornecedores cadastrados</h3>
 @endif
-
-
+ --}}

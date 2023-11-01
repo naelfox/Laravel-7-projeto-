f02c955f6f8c7fca9a60c22b4f243059e819bbdc
naelfox <natanaelvila2@gmail.com>
2023-10-31 22:00:39 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::middleware(LogAcessoMiddleware::class)->get('/', 'PrincipalController@principal')->name('site.index');
 
 Route::get('/sobre-nos',  'SobreNosController@sobreNos')->name('site.sobrenos');
 

fed451dfaf5b828780baa63e17729cc2bbf825db
naelfox <natanaelvila2@gmail.com>
2023-11-07 20:39:27 -0300

    .

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -18,21 +18,28 @@
         </div>
 
         <div class="informacao-pagina">
-            {{ $msg ?? '' }}
+
             <div style="width: 30%; margin-left: auto; margin-right: auto">
                 <form action="" method="post">
                     @csrf
-                    <input type="hidden" name="id" value="{{ $fornecedor->id ?? '' }}">
-                    <input type="text" name="nome" placeholder="Nome" value="{{ $fornecedor->nome ?? old('nome') }}" id="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-                    <input type="text" name="site" placeholder="Site" value="{{ $fornecedor->site ?? old('site') }}" id="borda-preta">
-                    {{ $errors->has('site') ? $errors->first('site') : '' }}
+                    <input type="hidden" name="id" value="">
+                    <input type="text" name="nome" placeholder="Nome" value="" id="borda-preta">
 
-                    <input type="text" name="uf" placeholder="UF" value="{{  $fornecedor->uf ?? old('uf') }}" id="borda-preta">
-                    {{ $errors->has('uf') ? $errors->first('uf') : '' }}
 
-                    <input type="text" name="email" placeholder="E-mail" value="{{ $fornecedor->email ?? old('email') }}" id="borda-preta">
-                    {{ $errors->has('email') ? $errors->first('email') : '' }}
+                    <input type="text" name="descricao" placeholder="Descrição" value="" id="borda-preta">
+
+
+
+                    <input type="text" name="peso" placeholder="Peso" value="" id="borda-preta">
+
+
+                    <select name="" id="">
+                        <option selected disabled>Selecione a unidade de medida</option>
+                        <option value="1">Unidade</option>
+                    </select>
+
+
+
 
                     <button type="submit" id="borda-preta">Cadastrar</button>
                 </form>
